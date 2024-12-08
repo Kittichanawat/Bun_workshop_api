a0bf9a463161474afb2fdad5c983afdc762d14d8
Kittichanawat <ratchakitti2546@gmail.com>
2024-12-08 21:08:22 +0700

    device

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import {jwt} from "@elysiajs/jwt";
 import {cors} from "@elysiajs/cors";
 import { UserController } from "./controllers/UserController";
-
+import { DeviceController } from "./controllers/DeviceController";
 const app = new Elysia()
 .use(jwt({
   name:'jwt',
@@ -14,6 +14,14 @@ const app = new Elysia()
 .get("/",()=>"Hello Elysia")
 .post("/api/user/signin",UserController.signIn)
 .put("/api/user/update",UserController.update)
+
+
+//device
+.post("/api/device/create",DeviceController.create)
+.get("/api/device/list",DeviceController.list)
+.put("/api/device/update/:id",DeviceController.update)
+.delete("/api/device/remove/:id",DeviceController.remove)
+
 .listen(3001);
 
 console.log(
